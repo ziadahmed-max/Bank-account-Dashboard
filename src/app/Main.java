0fbf7809f92d9e7ceff4e4833f21cab7b36fc5c1
Mahmoud-SamyEG <mahmoudsamyxyz510@gmail.com>
2026-05-10 12:33:50 +0200

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package app;
 
-/**
- *
- * @author Mahmoud Samy
- */
+
 public class Main {
     
+    public static void main(String[] args)
+    {
+        
+        
+    }
+   
 }
