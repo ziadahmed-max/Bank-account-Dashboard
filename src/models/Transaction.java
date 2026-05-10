ca0932493aca3e0c626176756baaae1e53d72572
Mahmoud-SamyEG <mahmoudsamyxyz510@gmail.com>
2026-05-10 12:31:33 +0200

    Update Transaction.java

--- src/models/Transaction.java
+++ src/models/Transaction.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package models;
 
-/**
- *
- * @author Mahmoud Samy
- */
+package models;
+import java.time.LocalDateTime;
+
+
 public class Transaction {
+    
+    //Attributes:
+    private String type; // Deposit or Withdraw
+    private double amount; // value of transaction
+    private LocalDateTime date; // time of transaction
+    
+    //Constructor:
+    public Transaction(String type, double amount)
+    {
+        this.type = type;
+        this.amount = amount;
+        this.date = LocalDateTime.now();
+    }
+
+    
+    //Getters
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+    
+    
+    @Override
+    public String toString()
+    {
+        return "Transaction Type: " + type + ", Amount: " + amount + ", Date:" + date;
+    }
+    
     
 }
