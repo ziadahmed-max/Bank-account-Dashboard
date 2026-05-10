8dde0b7e1f4f52c3e14d0ffb2d9cce8e16de6bc4
YousefTayyan <your@email.com>
2026-05-10 19:56:55 +0300

    Add SavingsAccount and CheckingAccount

---
+++ src/models/SavingsAccount.java
@@ -0,0 +1,148 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaces;
+
+import models.BankAccount;
+import models.Transaction;
+
+/**
+ *
+ * @author HP VICTUS
+ */
+public class SavingsAccount  extends BankAccount implements Auditable ,Printable{
+    
+    
+ private double interestRate ;
+ 
+ 
+ 
+ // constructor for savings vlass
+    public SavingsAccount(String accountNumber, String ownerName, double balance , double interestRate) {
+        super(accountNumber, ownerName, balance);
+        this.interestRate = interestRate ;
+        
+        
+    }
+    
+    public double getInterestRate(){
+    
+    return interestRate ;
+    }
+    
+    
+    public void setInterestRate ( double interestRate){
+     
+    this.interestRate = interestRate ;  
+        
+    }
+    
+    
+    
+    
+
+    public void applyInterest(){
+    
+    double interest =  balance * interestRate / 100 ;
+    balance += interest ;
+    System.out.println(interest + " Interest applied");
+    }     
+    
+    
+    
+    
+    
+    
+    
+    @Override 
+    public void deposit(double amount){
+        
+     if( amount<= 0){   
+         System.out.println(" Invalid deposit ");
+     }    
+     else{
+       balance += amount  ;
+       transactions.add( new Transaction ( "Deposit " , amount )) ;
+       
+         
+     }} 
+    
+    
+    
+    
+    
+    
+    @Override 
+    public void withdraw (double amountWithdrawn){
+        if(amountWithdrawn <= 0){
+        System.out.println(" Invalid withdraw");
+        
+        } else if(amountWithdrawn <= balance ){
+        balance -= amountWithdrawn ;
+        transactions.add( new Transaction ("Withdraw ",  amountWithdrawn));
+        
+        }else {
+        System.out.println("Insufficent Balnce");
+   
+        
+        }}
+
+        
+    
+   @Override
+
+       public void printDetails(){
+       
+       System.out.println(" ==== Savings Account ====");
+       System.out.println(" Account Number :" + accountNumber);
+       System.out.println("Owner : "+ ownerName);
+       System.out.println("Balance : "+ balance);
+       System.out.println("Interestrate : " +interestRate);
+       
+       }
+       
+       
+    @Override
+    
+       public void logAction( String action ){
+       
+       System.out.println("[LOG]" + action + " |Account:|" + accountNumber);
+       
+       }
+        
+       
+       
+       
+       @Override
+     public void applyMonthlyUpdate(){
+     
+         applyInterest();
+            System.out.println("Monthly update is applied for" + accountNumber);
+     
+     }
+
+   
+        
+
+    @Override
+    public void generateStatement() {
+      printDetails();
+      System.out.println(" === Transactions === ");
+    
+      for( Transaction t : transactions){
+          System.out.println(t.getType() + " : " + t.getAmount());
+          
+      }}
+       
+    
+        
+    
+    
+    
+        
+        
+        
+        
+    }
+    
